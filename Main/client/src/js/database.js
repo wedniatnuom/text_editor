09ca04a3f0f726a6a9d240cf2470f8cf91ec6ecb
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-28 13:00:47 -0600

    cleaning up code

--- Main/client/src/js/database.js
+++ Main/client/src/js/database.js
@@ -12,8 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-
 export const putDb = async (content) => {
   const touchDB = await openDB('contact', 1);
   const transDB = touchDB.transaction('contact', 'readwrite');
@@ -22,7 +20,6 @@ export const putDb = async (content) => {
   const result = await request
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const touchDB = await openDB('contact', 1);
   const transDB = touchDB.transaction('contact', 'readonly');
--- Main/client/webpack.config.js
+++ Main/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,16 +12,19 @@ module.exports = () => {
       editor: './src/js/editor.js',
       header: './src/js/header.js',
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
 
+      
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
